b61e6b44d32f625d9ae8799b6113c5e19b4789b9
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-13 14:34:19 +0300

    Added new method and updated old one

--- lib/repo/Base.dart
+++ lib/repo/Base.dart
@@ -1,9 +1,24 @@
+import 'dart:convert';
+
+import 'package:weather_app/models/location.dart';
 import 'package:weather_app/models/weather.dart';
 import 'package:weather_app/repo/IBase.dart';
+import 'package:dio/dio.dart' as dio;
 
-class Base implements IBase{
+class Base implements IBase {
   @override
-  List<Weather> getWeathers() {
+  Future<List<Weather>> getWeathers(String locationKey) async {
     return [];
+  }
+
+  @override
+  Future<Location> getLocation(String value) async {
+    final response = await dio.Dio().get(
+        'http://dataservice.accuweather.com/locations/v1/cities/search?apiKey=YgICKyx4wDUZhWblusDfM9HTeIHzq6Wt',
+        queryParameters: {
+          'q': value
+        });
+    final result = response.data as List<dynamic>;
+    return Location.fromJson(result.first);
   }
 }
