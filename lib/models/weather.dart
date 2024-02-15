8c0f3baa037865dd3e91aa936ab983de1f75dc26
Abdullah Halid USLU <a.halid.uslu@gmail.com>
2024-02-15 17:35:30 +0300

    Class filled

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -1,3 +1,22 @@
-class Weather{
 
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
+class Weather{
+  @JsonKey(name: 'Date')
+  final String date;
+  @JsonKey(name: 'Temperature')
+  final Map<String, dynamic> temperature;
+  @JsonKey(name: 'Day')
+  final Map<String, dynamic> day;
+  @JsonKey(name: 'Night')
+  final Map<String, dynamic> night;
+
+  Weather({required this.date, required this.temperature, required this.day, required this.night});
+
+  factory Weather.fromJson(Map<String, dynamic> json) => _$WeatherFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherToJson(this);
 }
